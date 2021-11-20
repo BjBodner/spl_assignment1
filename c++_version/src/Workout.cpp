9932caebd1d4d87baa82838417b0dbb1690af15e
BjBodner <benjybo7@gmail.com>
2021-11-20 09:07:05 +0200

    Added workout class

--- c++_version/src/Workout.cpp
+++ c++_version/src/Workout.cpp
@@ -1,19 +1,27 @@
 #include "../include/Workout.h"
 using namespace std
 Workout::Workout(int w_id, string w_name, int w_price, WorkoutType w_type):id(w_id),name(w_name),price(w_price),type(w_type) {}
-
-//TODO: VALIDATE IMPLEMENTATION
 Workout::Workout():id(0),name(""),price(0),type(CARDIO){}
-
-
-//TODO: MUST IMPLEMENT
+Workout::Workout(const Workout &other):{
+    id = other.id + 1
+    name = other.name
+    price = other.price
+    type = other.type
+}
 Workout &Workout::operator=(const Workout &other) {
-
-
-
+    /// Note, we might get an error here, since these fields are supposed to be const
+    id = other.id + 1
+    name = other.name
+    price = other.price
+    type = other.type
     return *this;
 }
-
+Workout::~Workout() {
+    delete id;
+    delete name;
+    delete price;
+    delete type;
+}
 //Getters
 
 string Workout::getName() const {
